f6ab55cb1cacd1ba1cf23871d3f4fa68b79a2d77
Tony <bernwey@gmail.com>
2017-09-03 11:20:41 +0800

    modify javasimple

--- laputa/monitor/src/main/java/com/sharemeng/laputa/monitor/JavaSimple.java
+++ laputa/monitor/src/main/java/com/sharemeng/laputa/monitor/JavaSimple.java
@@ -1,8 +1,11 @@
 package com.sharemeng.laputa.monitor;
 
 
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
 import io.prometheus.client.exporter.MetricsServlet;
+import io.prometheus.client.exporter.PushGateway;
 import io.prometheus.client.hotspot.DefaultExports;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -13,28 +16,63 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-import static sun.net.www.protocol.http.AuthCacheValue.Type.Server;
+import java.util.Random;
 
 /**
  * Created by Tony on 2017/8/30.
  */
 public class JavaSimple {
+    static CollectorRegistry registry = new CollectorRegistry();
+
     static class ExampleServlet extends HttpServlet {
         static final Counter requests = Counter.build()
                 .name("hello_worlds_total")
                 .help("Number of hello worlds served.").register();
 
+
         @Override
         protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
                 throws ServletException, IOException {
-            resp.getWriter().println("Hello World!");
+            resp.getWriter().println("Hello World! Push Gateway!");
             // Increment the number of requests.
             requests.inc();
+
+            // test push
+            try {
+                executeBatchJob();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        //PushGateway test
+        void executeBatchJob() throws Exception {
+
+            Gauge duration = Gauge.build()
+                    .name("my11_batch_job_duration_seconds").help("Duration of my batch job in seconds.").register(registry);
+            Gauge.Timer durationTimer = duration.startTimer();
+            try {
+                // Your code here.
+                Thread.sleep(new Random().nextInt(1000));
+
+                // This is only added to the registry after success,
+                // so that a previous success in the Pushgateway isn't overwritten on failure.
+                Gauge lastSuccess = Gauge.build()
+                        .name("my11_batch_job_last_success").help("Last time my batch job succeeded, in unixtime.").register(registry);
+                lastSuccess.setToCurrentTime();
+
+                Counter reqs = Counter.build().name("my11_batch_job_total").help("Request number of my batch job.").register(registry);
+                reqs.inc();
+            } finally {
+                durationTimer.setDuration();
+                PushGateway pg = new PushGateway("127.0.0.1:9091");
+                pg.pushAdd(registry, "my11_batch_job");
+            }
         }
     }
 
-    public static void main( String[] args ) throws Exception {
+    public static void main(String[] args) throws Exception {
         Server server = new Server(1234);
         ServletContextHandler context = new ServletContextHandler();
         context.setContextPath("/");
